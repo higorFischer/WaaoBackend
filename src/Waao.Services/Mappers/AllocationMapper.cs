17e8f7e8e2c568edb41d94dcd7f4c3f6ebd88e47
higorflopes <higor@waao.com.br>
2026-05-29 10:47:00 -0300

    feat(allocation): add AllocationMapper

---
+++ src/Waao.Services/Mappers/AllocationMapper.cs
@@ -0,0 +1,40 @@
+using Waao.Domain.Models.Entities;
+using Waao.Domain.Models.Entities.Allocation;
+using Waao.Services.Abstractions.Dtos.Allocation;
+
+namespace Waao.Services.Mappers;
+
+public static class AllocationMapper
+{
+	public static CollaboratorChipDto ToChip(Collaborator c) => new()
+	{
+		Id = c.Id,
+		FullName = c.FullName,
+		PhotoUrl = c.PhotoUrl,
+		RoleTitle = c.Role != null ? c.Role.Title : null,
+		DepartmentName = c.Department != null ? c.Department.Name : null,
+	};
+
+	public static AllocationDto ToDto(ProjectAllocation a) => new()
+	{
+		Id = a.Id,
+		ProjectId = a.ProjectId,
+		Note = a.Note,
+		Position = a.Position,
+		AllocatedAt = a.AllocatedAt,
+		Collaborator = ToChip(a.Collaborator),
+	};
+
+	public static ProjectWithAllocationsDto ToDto(Project p) => new()
+	{
+		Id = p.Id,
+		Title = p.Title,
+		Description = p.Description,
+		ColorHex = p.ColorHex,
+		Position = p.Position,
+		Allocations = p.Allocations
+			.OrderBy(a => a.Position)
+			.Select(ToDto)
+			.ToList(),
+	};
+}
