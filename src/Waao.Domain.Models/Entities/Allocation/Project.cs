b86e5feb015614f85192a8f42dca7f6bc1f302d5
higorflopes <higor@waao.com.br>
2026-05-29 10:43:00 -0300

    feat(allocation): add Project and ProjectAllocation entities

---
+++ src/Waao.Domain.Models/Entities/Allocation/Project.cs
@@ -0,0 +1,26 @@
+namespace Waao.Domain.Models.Entities.Allocation;
+
+public class Project : Entity
+{
+	public string Title { get; set; } = string.Empty;
+	public string? Description { get; set; }
+	public string ColorHex { get; set; } = "#2A6B7E";
+	public int Position { get; set; }
+	public bool IsArchived { get; set; }
+
+	public virtual ICollection<ProjectAllocation> Allocations { get; set; } = [];
+}
+
+public class ProjectAllocation : Entity
+{
+	public Guid ProjectId { get; set; }
+	public virtual Project Project { get; set; } = null!;
+
+	public Guid CollaboratorId { get; set; }
+	public virtual Collaborator Collaborator { get; set; } = null!;
+
+	public string? Note { get; set; }
+	public int Position { get; set; }
+	public DateTime AllocatedAt { get; set; } = DateTime.UtcNow;
+	public Guid? AllocatedById { get; set; }
+}
